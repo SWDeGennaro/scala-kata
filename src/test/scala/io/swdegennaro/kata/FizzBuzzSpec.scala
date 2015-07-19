80c44bec76e75a3f7401ee709d982dc4b18aed31
SWDeGennaro <ste.degennaro@live.co.uk>
2015-07-19 19:47:16 +0100

    Updating formating

--- src/test/scala/io/swdegennaro/kata/FizzBuzzSpec.scala
+++ src/test/scala/io/swdegennaro/kata/FizzBuzzSpec.scala
@@ -4,22 +4,22 @@ import org.scalatest.{Matchers, FlatSpec}
 
 class FizzBuzzSpec extends FlatSpec with Matchers {
 
-	"FizzBuzz" should "return fizz if the number is devisible by 3" in {
-		FizzBuzz.getResult(3) should be ("fizz")
-		FizzBuzz.getResult(6) should be ("fizz")		
-	}
+  "FizzBuzz" should "return fizz if the number is devisible by 3" in {
+    FizzBuzz.getResult(3) should be ("fizz")
+    FizzBuzz.getResult(6) should be ("fizz")		
+  }
 
-	it should "return buzz if the number is devisible by 5" in {
-		FizzBuzz.getResult(5) should be ("buzz")
-		FizzBuzz.getResult(10) should be ("buzz")
-	}
+  it should "return buzz if the number is devisible by 5" in {
+    FizzBuzz.getResult(5) should be ("buzz")
+    FizzBuzz.getResult(10) should be ("buzz")
+  }
 
-	it should "return fizzbuzz if the number is devisible by 5 and 3" in {
-		FizzBuzz.getResult(15) should be ("fizzbuzz")	
-		FizzBuzz.getResult(30) should be ("fizzbuzz")	
-	}
+  it should "return fizzbuzz if the number is devisible by 5 and 3" in {
+    FizzBuzz.getResult(15) should be ("fizzbuzz")	
+    FizzBuzz.getResult(30) should be ("fizzbuzz")	
+  }
 
-	it should "return the number if there is no match" in {
-		FizzBuzz.getResult(7) should be ("7")
-	}
+  it should "return the number if there is no match" in {
+    FizzBuzz.getResult(7) should be ("7")
+  }
 }
