aa7816efaf437fdb700e382390e680955f7972ec
SWDeGennaro <ste.degennaro@live.co.uk>
2015-07-19 19:48:10 +0100

    Updating formating

--- src/main/scala/io/swdegennaro/kata/FizzBuzz.scala
+++ src/main/scala/io/swdegennaro/kata/FizzBuzz.scala
@@ -2,12 +2,10 @@ package io.swdegennaro.kata
 
 object FizzBuzz {
 	
-	def getResult(number: Int): String = (number % 3, number % 5) match {
-		case (0, 0) => "fizzbuzz"
-		case (0, _) => "fizz"
-		case (_, 0) => "buzz"
-		case (_, _) => number.toString()
-	}
+  def getResult(number: Int): String = (number % 3, number % 5) match {
+    case (0, 0) => "fizzbuzz"
+    case (0, _) => "fizz"
+    case (_, 0) => "buzz"
+    case (_, _) => number.toString()
+  }
 }
-
-
